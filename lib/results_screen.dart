6361e5935feda5ba1255153e96a8ada6eb09a4c8
Jeremy Vollant <j.vollant@gmail.com>
2024-05-20 23:18:09 +0200

    correction

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_quizz/data/questions.dart';
-import 'package:flutter_quizz/questions_summary.dart';
+import 'package:flutter_quizz/questions_summary/questions_summary.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ResultsScreen extends StatelessWidget {
